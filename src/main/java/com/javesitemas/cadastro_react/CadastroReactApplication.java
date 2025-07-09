81715718da940354ac564496f91d392f3db3f4c7
Yuri Fernandes <devyuri.programador@gmail.com>
2025-07-08 21:44:42 -0300

    iniciando o back

---
+++ src/main/java/com/javesitemas/cadastro_react/CadastroReactApplication.java
@@ -0,0 +1,13 @@
+package com.javesitemas.cadastro_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javesitemas/cadastro_react/CadastroReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javesitemas.cadastro_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
